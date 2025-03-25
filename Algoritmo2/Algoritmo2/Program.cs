98e3fa3f4f19c4478dfded81eade04583ee0204c
Leonardo4Schneider <leonardoschneider4096@gmail.com>
2025-03-25 20:53:47 -0300

    adicionado var altura

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -2,4 +2,5 @@
 string nomeCliente = "Leonardo Silva";
 decimal preco = 20M;
 char letra = 'a';
-var nomecompleto = "Leonardo da Silva Schneider"
+var nomecompleto = "Leonardo da Silva Schneider";
+var altura = 175;
